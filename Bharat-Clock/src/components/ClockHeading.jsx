0a0b666affc1fb3c41280f966b99311c3652d21a
rdroid-27 <gupta27rachit@gmail.com>
2024-01-15 13:34:04 +0530

    Added Basic UI of Bharat Clock Project.

---
+++ Bharat-Clock/src/components/ClockHeading.jsx
@@ -0,0 +1,8 @@
+let ClockHeading= ()=>{
+
+  return (
+    <h1 className="fw-bold">Bharat Clock</h1>
+  )
+
+}
+export default ClockHeading;
---
+++ Bharat-Clock/src/components/ClockSlogan.jsx
@@ -0,0 +1,8 @@
+let ClockSlogan= ()=>{
+
+  return (
+    <p className="lead">This is the clock that shows the current time in Bharat at all times</p>
+  )
+
+}
+export default ClockSlogan;
